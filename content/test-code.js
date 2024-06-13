f7ec5c571740b95e5dde278b8ebc5832563cf835
browser tests <browsertests@localhost>
2024-06-13 09:45:43 -0700

    "Autosave"

--- content/test-code.js
+++ content/test-code.js
@@ -26,8 +26,6 @@ function draw() {
     rect(20, 10, 5, 20);
   }
 
-  translate(width / 2, height / 2);
-
   const interval_s = frameDelta / ms_per_s;
   const interval_h = interval_s / s_per_hour;
   const [x, y, z] = localFrame.roll;
